19e73dc379a867c31afed5cefce7904274720ac0
parthchaudhary <parthchaudhary69@gmail.com>
2021-03-20 11:55:03 +0530

    teacher courses design and classes home page design

--- js/script.js
+++ js/script.js
@@ -4,4 +4,44 @@ $(document).ready(function(){
         arrows: false,
         dots: true
     });
+
+    let mainMinusHeight = Math.round($('header').outerHeight(true) + $('footer').outerHeight(true) + $('.signup-sec').outerHeight(true));
+    
+    $('main').attr("style",'--minusHeight:' + `${mainMinusHeight}px`);
+
+    // accordion call
+    accordion();
 });
+
+function accordion() {
+    $(".accordion-header").click(function() {
+      if (
+        $(this)
+        .next()
+        .is(":visible")
+      ) {
+        $(this)
+          .next()
+          .slideUp();
+        $(this)
+          .parent()
+          .addClass("accordion-close");
+        $(this)
+          .parent()
+          .removeClass("accordion-open");
+      } else {
+        $(".accordion-body").slideUp();
+        $(".accordion-item").removeClass("accordion-open");
+        $(this)
+          .next()
+          .slideDown();
+        $(this)
+          .parent()
+          .addClass("accordion-open");
+        $(this)
+          .parent()
+          .removeClass("accordion-close");
+      }
+    });
+}
+  
